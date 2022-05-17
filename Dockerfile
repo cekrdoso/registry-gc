099c0f047c2c90dfbc27b8439b6daffc5137432e
Carlos Eduardo Cardoso <cekrdoso@gmail.com>
2022-05-17 19:15:37 -0300

    Read variables from docker secrets

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM registry:2.7
 
 COPY docker-entrypoint.sh /entrypoint.sh
 
-COPY exec-garbage-collect.sh /exec-garbage-collect.sh 
+COPY exec-garbage-collect.sh /exec-garbage-collect.sh
 
-RUN apk --no-cache add tzdata \
-  && chmod +x /entrypoint.sh /exec-garbage-collect.sh 
+RUN apk --no-cache add bash tzdata \
+  && chmod +x /entrypoint.sh /exec-garbage-collect.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo "Both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+# Evaluate environment variables
+for EV in $(env | grep -oE '^REGISTRY_[^=]+'); do
+    file_env ${EV%%_FILE}
+done
 
 case "$1" in
     *.yaml|*.yml)
-      # Garbage collect routine
-      # Will exit when garbage collect delete something
-      /exec-garbage-collect.sh 2>&1 &
-      set -- registry serve "$@"
+        # Garbage collect routine
+        # Will exit when garbage collect delete something
+        /exec-garbage-collect.sh 2>&1 &
+
+        set -- registry serve "$@"
     ;;
     serve|garbage-collect|help|-*) set -- registry "$@" ;;
 esac
